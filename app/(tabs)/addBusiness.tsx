30ae9e9c7efea7687f9518d8e40fe6e13b712216
shiva prasad <agustya742@gmail.com>
2025-01-08 19:47:54 +0530

    dropdown added in addBusiness page

--- app/(tabs)/addBusiness.tsx
+++ app/(tabs)/addBusiness.tsx
@@ -1,37 +1,54 @@
 import CustomButton from '@/components/CustomButton'
+import DropdownComponent from '@/components/dropdown'
 import CustomHeader from '@/components/Header'
 import InputField from '@/components/InputField'
 import { Entypo, FontAwesome } from '@expo/vector-icons'
 import { router } from 'expo-router'
 import React, { useState } from 'react'
 import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native'
-import ReactNativeModal from 'react-native-modal'
+import ReactNativeModal from 'react-native-modal';
+
+  const data = [
+    { label: 'Event Hall', value: '1' },
+    { label: 'Props', value: '2' },
+    { label: 'Photos & Videos', value: '3' },
+    { label: 'Decorations', value: '4' },
+    { label: 'Music & DJ', value: '5' },
+    { label: 'MUA', value: '6' },
+    { label: 'Food & Beverages', value: '7' },
+    { label: 'Invitations', value: '8' },
+    { label: 'Arificial Jewels', value: '9' },
+    { label: 'Vehical Convoie', value: '10' },
+  ];
+
 
 const addBusiness = () => {
     const [showSuccessModal, setShowSuccessModal] = useState(false);
+    const [value, setValue] = useState(null);
     return (
         <>
           <SafeAreaView>
             <CustomHeader title="Add Business" home={false} backArrow={true} onBack={() => router.back()}/>
             <ScrollView className=' bg-[#efefef] h-screen'>
               <View className='flex w-full pb-28'>
+                <View className='flex px-5 bg-white w-full  pt-5'>
+                    <Text className='mb-5 text-sm'>Business Type</Text>
+                    <DropdownComponent data={data} value={value}  />
+                </View>
                 <View className='flex px-5 pt-5 bg-white w-full'>
-                    <InputField label="Business Name" placeholder='Title' labelStyle="text-sm" inputStyle="h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
+                    <InputField label="Business Name" placeholder='Title' labelStyle="text-sm" inputStyle="h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-600'/>
                 </View>
                 <View className='flex px-5 bg-white w-full'>
-                    <InputField label="Business Type" placeholder='Category' labelStyle="text-sm"  inputStyle="h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
+                    <InputField label="city" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-600'/>
                 </View>
                 <View className='flex px-5 bg-white w-full'>
-                    <InputField label="city" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
+                    <InputField label="State" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-600'/>
                 </View>
                 <View className='flex px-5 bg-white w-full'>
-                    <InputField label="State" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
+                    <InputField label="Country" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-600'/>
                 </View>
                 <View className='flex px-5 bg-white w-full'>
-                    <InputField label="Country" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
-                </View>
-                <View className='flex px-5 bg-white w-full'>
-                    <InputField label="Address" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
+                    <InputField label="Address" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-600'/>
                 </View>
                 <View className='flex px-5 bg-white w-full'>
                     <Text className='mb-5 text-sm'>Location</Text>
@@ -39,9 +56,9 @@ const addBusiness = () => {
                     {/* <InputField label="Location" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/> */}
                 </View>
                 <View className='flex px-5 bg-white w-full border-b-[0.5px] border-gray-300'>
-                    <InputField label="Description" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-300'/>
+                    <InputField label="Description" placeholder='' labelStyle="text-sm" inputStyle=" h-[35px] text-[14px] rounded-sm" containerStyle='border-[0.5px] border-gray-600'/>
                 </View>
-                <View className='flex px-5 pt-5 bg-white w-full border-b-[0.5px] border-gray-300'>
+                <View className='flex px-5 pt-5 bg-white w-full border-b-[0.5px] border-gray-600'>
                     <View className='flex rounded-lg ring-1 ring-gray-300 bg-sky-200 p-5 justify-center items-center mb-5'>
                         <FontAwesome name="image" size={34} color="#0ea5e9" />
                         <Text className='text-xl font-bold mt-3'>Upload Photos</Text>
